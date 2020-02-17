3e1f1c5f8251ec3ff5923dccdd26702f81ac4d79
compucter <egor-ruban@mail.ru>
2020-02-17 17:10:09 +0700

    новая верстка

--- app/src/main/java/ru/tsu/huffman/MainActivity.kt
+++ app/src/main/java/ru/tsu/huffman/MainActivity.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.codekidlabs.storagechooser.StorageChooser
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_mainl.*
 import java.io.*
 
 
@@ -15,47 +16,41 @@ class MainActivity : AppCompatActivity() {
 
     val LOG_TAG = "myLogs"
 
-    val FILENAME = "testFile"
+    var fileToRead = "default.txt"
+    var fileToWrite = fileToRead
 
-    val DIR_SD = "MyFiles"
-    val FILENAME_SD = "fileSD"
-
-    var file = FILENAME
-    var fileToWrite = file
-
-
-
-    /** Called when the activity is first created.  */
     override fun onCreate(savedInstanceState: Bundle?) {
-        file = "${baseContext.filesDir}/$file"
+        fileToRead = "$filesDir/$fileToRead"
+        fileToWrite = fileToRead
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Log.d(LOG_TAG, "start")
+        setContentView(R.layout.activity_mainl)
 
-        btnWrite.setOnClickListener {
-            val data = byteArrayOf(48, 49, 50, -1, 0, 51, 10, 52)
+        btn_code.setOnClickListener {
+            val data = byteArrayOf(48, 49, 50, -1, 0, 51, 10, 52) // пример данных
             writeFile(byteArrayToCharArray(data))
         }
-        btnRead.setOnClickListener {
+
+        btn_decode.setOnClickListener {
             readFile()
         }
-        btnFilePicker.setOnClickListener {
-            val chooser = StorageChooser.Builder() // Specify context of the dialog
+
+        btn_read.setOnClickListener {
+            val chooser = StorageChooser.Builder()
                 .withActivity(this)
                 .withFragmentManager(fragmentManager)
                 .withMemoryBar(true)
-                .allowCustomPath(true) // Define the mode as the FILE CHOOSER
+                .allowCustomPath(true)
                 .setType(StorageChooser.FILE_PICKER)
                 .build()
             chooser.setOnSelectListener { path ->
                 if(".txt" in path){
                     Toast.makeText(this, "txt", Toast.LENGTH_SHORT).show()
                     Log.i("MY", path) // что делать с выбранным элементом
-                    file = path
+                    fileToRead = path
                     val a = getDir("Huffman", Context.MODE_PRIVATE).createNewFile()
                     Log.d(LOG_TAG, "${File(path).name} $path and $a")
                     fileToWrite = "${File(path).parent}/${File(path).name.replace(".txt", ".hfm")}"
-
+                    readFile()
 
                 } else {
                     Toast.makeText(this,"wrong type",Toast.LENGTH_SHORT).show()
@@ -81,9 +76,6 @@ class MainActivity : AppCompatActivity() {
         val ba = ByteArray(data.length)
         for(i in data.indices){
             ba[i] = data[i].toByte()
-        }
-        for(i in ba.indices){
-            //Log.d(LOG_TAG, "ba is ${ba[i]}")
         }
         return ba
     }
@@ -127,7 +119,7 @@ class MainActivity : AppCompatActivity() {
             //открываем поток для чтения
           // val fileData = ByteArray(file.length().toInt())
             Log.d(LOG_TAG, "psh")
-            val f = File(file)
+            val f = File(fileToRead)
             val br = BufferedReader(
                 InputStreamReader(
                     FileInputStream(f)
@@ -137,6 +129,8 @@ class MainActivity : AppCompatActivity() {
             var str: String = br.readText()
             // читаем содержимое
             Log.d(LOG_TAG, "read all $str")
+            tv_file.visibility = View.VISIBLE
+            tv_file.text = str
             stringToByteArray(str)
             //while (br.readLine().also({ str = it }) != null) {
             //  Log.d(LOG_TAG, str)
